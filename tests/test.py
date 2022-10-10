4ddf6d963153539c8121ab046ce1e5223f352cab
Simon Miszewski <simonmiszewski@Simons-iMac.local>
2022-10-10 19:43:41 +0200

    add new test

--- tests/test.py
+++ tests/test.py
@@ -8,3 +8,4 @@ def test_top_n():
     assert myModule.top_n([8,3,2,7,4],3) == [8,7,4], 'incorrect'
     assert myModule.top_n([10,1,12,9,2],2) == [12,10], 'incorrect'
     assert myModule.top_n([1,2,3,4,5],5) == [5,4,3,2,1], 'incorrect'
+    assert myModule.top_n([10,9,8,7,6],1) == [10], 'incorrect'
